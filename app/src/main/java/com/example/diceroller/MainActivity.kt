43a1925935072367869c7d87c237a631989b6ba5
raashidbaba <raashidbaba849@gmail.com>
2023-01-21 00:11:08 +0530

    added some good coding practice.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,8 +20,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomNumb = findViewById<TextView>(R.id.tvOne)
-        val random = Random.nextInt(6) + 1
-        randomNumb.text = random.toString()
+         diceImage = findViewById(R.id.diceImage)
+      //  val random = Random.nextInt(6) + 1
+        val random = (1..6).shuffled().first()
+        val randomDiceImg = when(random){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(randomDiceImg)
+
+
     }
 }
